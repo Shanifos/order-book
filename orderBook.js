dd486f8b954bdc515deb18c3ed7cf6db5677ddad
Shani Foshee <shanifoshee@Shanis-Air.hsd1.ma.comcast.net>
2019-11-05 10:32:33 -0500

    added nested if/else statements

--- orderBook.js
+++ orderBook.js
@@ -1,40 +1,32 @@
 function reconcileOrder(existingBook, incomingOrder) {
   if (existingBook.length === 0) {
     existingBook.push(incomingOrder)
+  } else {
+    existingBook.forEach(existingOrders => {
+      if (existingOrders.type !== incomingOrder.type) {
+        if (existingOrders.quantity === incomingOrder.quantity) {
+          if (existingOrders.price < incomingOrder.price) {
+            existingBook.push(incomingOrder)
+          } else {
+            existingBook.pop(existingOrders)
+          }
+        } else if (existingOrders.price === incomingOrder.price) {
+          if (incomingOrder.quantity < existingOrders.quantity) {
+            existingOrders.quantity =
+              existingOrders.quantity - incomingOrder.quantity
+          } else {
+            incomingOrder.quantity =
+              incomingOrder.quantity - existingOrders.quantity
+            existingBook.pop(existingOrders) && existingBook.push(incomingOrder)
+          }
+        } else {
+          existingBook.push(incomingOrder)
+        }
+      } else {
+        existingBook.push(incomingOrder)
+      }
+    })
   }
-  existingBook.forEach(eBook => {
-    if (
-      (eBook.price !== incomingOrder.price &&
-        eBook.quantity !== incomingOrder.quantity) ||
-      (eBook.quantity === incomingOrder.quantity &&
-        eBook.price < incomingOrder.price &&
-        eBook.type !== incomingOrder.type)
-    ) {
-      existingBook.push(incomingOrder)
-    }
-    if (
-      incomingOrder.quantity < eBook.quantity &&
-      incomingOrder.type !== eBook.type &&
-      eBook.price === incomingOrder.price
-    ) {
-      eBook.quantity = eBook.quantity - incomingOrder.quantity
-    } else if (
-      incomingOrder.quantity > eBook.quantity &&
-      incomingOrder.type !== eBook.type &&
-      eBook.price === incomingOrder.price
-    ) {
-      incomingOrder.quantity = incomingOrder.quantity - eBook.quantity
-      existingBook.pop(eBook) && existingBook.push(incomingOrder)
-    }
-    if (
-      eBook.type !== incomingOrder.type &&
-      eBook.quantity === incomingOrder.quantity &&
-      (eBook.price === incomingOrder.price || eBook.price > incomingOrder.price)
-    ) {
-      existingBook.pop(eBook)
-    }
-  })
-
   return existingBook
 }
 
