eddaf5ea45ac4be48fce242da929221cbfbce197
Shani Foshee <shanifoshee@Shanis-Air.hsd1.ma.comcast.net>
2019-11-04 20:27:41 -0500

    created a book order

--- orderBook.js
+++ orderBook.js
@@ -1,5 +1,41 @@
 function reconcileOrder(existingBook, incomingOrder) {
-  throw new Error('Not Implemented Yet')
+  if (existingBook.length === 0) {
+    existingBook.push(incomingOrder)
+  }
+  existingBook.forEach(eBook => {
+    if (
+      (eBook.price !== incomingOrder.price &&
+        eBook.quantity !== incomingOrder.quantity) ||
+      (eBook.quantity === incomingOrder.quantity &&
+        eBook.price < incomingOrder.price &&
+        eBook.type !== incomingOrder.type)
+    ) {
+      existingBook.push(incomingOrder)
+    }
+    if (
+      incomingOrder.quantity < eBook.quantity &&
+      incomingOrder.type !== eBook.type &&
+      eBook.price === incomingOrder.price
+    ) {
+      eBook.quantity = eBook.quantity - incomingOrder.quantity
+    } else if (
+      incomingOrder.quantity > eBook.quantity &&
+      incomingOrder.type !== eBook.type &&
+      eBook.price === incomingOrder.price
+    ) {
+      incomingOrder.quantity = incomingOrder.quantity - eBook.quantity
+      existingBook.pop(eBook) && existingBook.push(incomingOrder)
+    }
+    if (
+      eBook.type !== incomingOrder.type &&
+      eBook.quantity === incomingOrder.quantity &&
+      (eBook.price === incomingOrder.price || eBook.price > incomingOrder.price)
+    ) {
+      existingBook.pop(eBook)
+    }
+  })
+
+  return existingBook
 }
 
 module.exports = reconcileOrder
